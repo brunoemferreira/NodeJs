9146dac301fc5f05e3674a843986aff704dd8298
Bruno Eduardo de Moura Ferreira <brunoemf@gmail.com>
2024-01-21 13:57:24 -0300

    Rotas de criação e listagem (Métodos HTTP)

--- 01-fundamentos-nodejs/src/server.js
+++ 01-fundamentos-nodejs/src/server.js
@@ -1,7 +1,20 @@
 import http from 'node:http'
 
 const server = http.createServer((req, res) => {
-   return res.end('Hello Bruno')
+    const { method, url } = req
+
+
+    if (method === 'GET' && url === '/users') {
+        return res.end('Listagem de usuários')
+    }
+
+    if (method === 'POST' && url === '/users') {
+        return res.end('Criação de usuários')
+    }
+
+
+
+    return res.end('Hello Bruno')
 })
 
 server.listen(3333)
