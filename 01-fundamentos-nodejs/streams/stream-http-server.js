31085aff6a146710bac9b54651ba22c034ef436c
Bruno Eduardo de Moura Ferreira <brunoemf@gmail.com>
2024-01-22 15:35:40 -0300

    Consumindo uma stream completa

--- 01-fundamentos-nodejs/streams/stream-http-server.js
+++ 01-fundamentos-nodejs/streams/stream-http-server.js
@@ -1,8 +1,8 @@
 import http from 'node:http'
-import { Transform} from 'node:stream'
+import { Transform } from 'node:stream'
 
-class InverseNumberStream extends Transform{
-    _transform(chunk, encoding, callback){
+class InverseNumberStream extends Transform {
+    _transform(chunk, encoding, callback) {
         const transformed = Number(chunk.toString()) * -1
 
         console.log(transformed)
@@ -14,10 +14,21 @@ class InverseNumberStream extends Transform{
 // req => ReadableStream
 // res => WritableStream 
 
-const server = http.createServer((req, res) => {
-   return req 
-   .pipe(new InverseNumberStream())
-   .pipe(res)
+const server = http.createServer(async(req, res) => {
+    const buffers = []
+
+    for await (const chunk of req){
+        buffers.push(chunk)
+    }
+    
+    const fullStreamContent = Buffer.concat(buffers).toString()
+
+    console.log(fullStreamContent)
+
+    return res.end(fullStreamContent)
+    //return req 
+    //.pipe(new InverseNumberStream())
+    //.pipe(res)
 })
 
 server.listen(3334)
